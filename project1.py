d203801e49c3f0c45174c907ea4021163fa38cdb
Jack Platten <plttn@users.noreply.github.com>
2016-02-08 14:39:31 -0800

    Start mapping out function

--- project1.py
+++ project1.py
@@ -1,1 +1,17 @@
-print ("Hello world");
+for x in range(0, width):
+	for y in range(0,height):
+
+	# we have nine images
+	for imageNumber in range(0,9)
+
+	#JES getPixel function takes as input a picture object and coordinate pair
+
+	pixel = getPixel(pictures[imageNumber], x, y)
+
+	red = getRed(pixel)
+	green = getGreen(pixel)
+	blue = getBlue(pixel)
+
+	redPixelList.append(red)
+	greenPixelList.append(green)
+	bluePixelList.append(blue)
